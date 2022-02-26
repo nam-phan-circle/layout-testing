f05978d7dbae5995b4690d6864930467032978bd
Nam phan <80142688+nam-phan-circle@users.noreply.github.com>
2022-02-26 09:50:01 +0700

    fix bugs modal in mobile mode

--- components/pages/header.vue
+++ components/pages/header.vue
@@ -324,8 +324,8 @@ export default {
       bottom: 0;
       left: 0;
       background: rgba(0, 0, 0, 0.5);
-      width: 100%;
-      height: 100%;
+      width: 100vw;
+      height: 100vh;
       z-index: 9;
       display: flex;
       justify-content: center;
